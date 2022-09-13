8aef2e9df1a376e0e82d6672a4e1918e2da3872d
Leonard Contreras <leonard.contreras03@gmail.com>
2022-09-12 19:57:36 -0500

    working array of integers returns an array of 2

--- js/sum-sequence-numbers.js
+++ js/sum-sequence-numbers.js
@@ -1,5 +1,29 @@
+/**Given an array of integers, write a function that returns an array of 2 integers. The first integer will be the count of the positive integers, and the second integer will be the sum of the negative integers. If the input is empty or null, return an empty array.
+ **/
+var array = [1, -6, 5, 4, 3, -7, -10, 201, -3];
+let newArray = [];
+let sum = 0;
+let count = [];
+let noInput = []
+
+for (var i = 0; i < array.length; i++) {
+    if (array[i] < 0) {
+        sum += array[i];
+    }
+    if (array[i] > 0) {
+        count.push(i);
+    }
+    if (array.length === 0 || array[i] == null) {
+        noInput.push(i)
+    }
+}
+newArray.push(count.length);
+newArray.push(sum);
+console.log(newArray)
+
+
 /**Given a string of any length with any characters in it, write a function to determine whether or not the string contains the whole word "english". The order of characters/spelling is important, a string "agkjnenglishsad" would return true while "asdneglihsth" would return false. Upper and lower case does not matter. Return values should be booleans (true/false).
-**/
+ **/
 
 //Looks for the word english inside of string
 let string = "agkjnenglishsad";
@@ -15,33 +39,36 @@ console.log(newString.includes(newWord)); //false
 
 
 /** Write a function that returns the SUM of a sequence of numbers. This sequence is determined by three variables: start, finish, and step. The sequence begins at start, ends at finish, and goes up by step until it reaches finish or a number that when increased by step would go past finish.
-**/
+ **/
 //Min number is 1 max is 4 and increases by 1
 function sumAllNumbers(num1, num2) {
     let sum = 0
-    for (let i = num1; i <= num2; i+=1) {
-        sum +=i
+    for (let i = num1; i <= num2; i += 1) {
+        sum += i
     }
     return sum
 }
-console.log(sumAllNumbers(1,4)) //output 10
+
+console.log(sumAllNumbers(1, 4)) //output 10
 
 //Min number is 4, max is 10 increases by 4
 function sumAgain(num1, num2) {
     let sum = 0
-    for (let i = num1; i <= num2;i+=4) {
-        sum +=i
+    for (let i = num1; i <= num2; i += 4) {
+        sum += i
     }
     return sum
 }
-console.log(sumAgain(4,10)) //output 12
+
+console.log(sumAgain(4, 10)) //output 12
 
 //Min number is 7, max is 10 increases by 1
 function sumExample(num1, num2) {
     let sum = 0
-    for (let i = num1; i <= num2;i+=1) {
-        sum +=i
+    for (let i = num1; i <= num2; i += 1) {
+        sum += i
     }
     return sum
 }
-console.log(sumExample(7,10)) //output 34
+
+console.log(sumExample(7, 10)) //output 34
