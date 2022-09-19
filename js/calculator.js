0196f1c25cfdf8a6565fc5b69595f505d93d7026
Leonard Contreras <leonard.contreras03@gmail.com>
2022-09-19 14:43:27 -0500

    refactored the buttons to inputs to separate output

--- js/calculator.js
+++ js/calculator.js
@@ -1,2 +1,10 @@
 // let str = prompt('Please enter a word');
 // console.log(str);
+
+function display(value) {
+    document.getElementById("left-operand").value+=value;
+    if (value === "+" || value === "-") {
+        document.getElementById("operator").value+=value;
+    }
+}
+
