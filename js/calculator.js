1a9898978524c1bf0516d0e54bfb13e0f81359d7
Leonard Contreras <leonard.contreras03@gmail.com>
2022-09-19 15:07:37 -0500

    added operand functionality

--- js/calculator.js
+++ js/calculator.js
@@ -2,13 +2,17 @@
 // console.log(str);
 
 function display(value) {
-    document.getElementById("left-operand").value+=value;
-    // if (value === "+" || value === "-") {
-        // document.getElementById("operator").value+=value;
-    // }
+    // if (value===)
+    // document.getElementById("left-operand").value+=value;
+    if (value === "+" || value === "-" || value ==="*" || value === "/") {
+        document.getElementById("operator").value+=value;
+    } else {
+        document.getElementById("left-operand").value+=value;
+    }
 }
 
 function clearInput() {
     document.getElementById("left-operand").value="";
+    document.getElementById("operator").value="";
 }
 
