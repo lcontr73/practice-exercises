a4f132a8bf767b42c0ff0745c8b81f5f53c0e3fe
Leonard Contreras <leonard.contreras03@gmail.com>
2022-09-26 18:56:37 -0500

    created price for movie rentals logic

--- js/external.js
+++ js/external.js
@@ -7,4 +7,18 @@ var color = prompt('What is your favorite color?')
 
 alert('Great ' + color.toLowerCase() + ' is my favorite color too!')
 
+/**
+ Write some JavaScript code, that is, variables and operators, to describe the following scenarios. Do not worry about the real operations to get the values, the goal of these exercises is to understand how real world conditions can be represented with code.
+
+ You have rented some movies for your kids: The little mermaid (for 3 days), Brother Bear (for 5 days, they love it), and Hercules (1 day, you don't know yet if they're going to like it). If price for a movie per day is $3, how much will you have to pay?
+ **/
+
+var littleMermaidDays = prompt('How many days would you like to rent The Little Mermaid?');
+var brotherBearDays = prompt('How many days would you like to rent Brother Bear?');
+var herculesDays = prompt('How many days would you like to rent Hercules?');
+var pricePerDay = prompt('How how much does it cost to rent a movie for one day?');
+var priceForOneMoviePerDay = (parseFloat(pricePerDay).toFixed(2))
+
+alert('$' + (((priceForOneMoviePerDay)) * (parseInt(littleMermaidDays) + parseInt(brotherBearDays) + parseInt(herculesDays))).toFixed(2));
+
 
