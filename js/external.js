fbdee4fc65059507b4babb8fb6f0d9b9f4f46fcb
Leonard Contreras <leonard.contreras03@gmail.com>
2022-09-26 18:44:29 -0500

    created practice files for review or previous exercises

---
+++ js/external.js
@@ -0,0 +1,10 @@
+"use strict";
+console.log("Hello from external JavaScript");
+
+alert('Welcome to my Website');
+
+var color = prompt('What is your favorite color?')
+
+alert('Great ' + color.toLowerCase() + ' is my favorite color too!')
+
+
