acbddbc558ea2c1571631e7da8278fdc52fc5cbb
Leonard Contreras <leonard.contreras03@gmail.com>
2022-09-30 19:09:29 -0500

    refactored multiplication table to take input from a prompt and determine if the input is a number

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,12 +1,19 @@
 "use strict";
 
-
-function showMultiplicationTable (num1) {
-    var i = 1;
-    while(i <= 10) {
-        console.log(num1 + ' x ' + i + ' = ' + (num1 * i) );
-        i++
+//function to create a multiplication table up to 10 using a prompt to input a number
+let num1 = prompt('Please pick a number!!')
+function showMultiplicationTable(num2) {
+    let i = 1;
+    if (isNaN(num2) === true) {
+        console.log('Please try again!!')
+    } else
+    while (i <= 10) {
+        console.log(num2 + ' x ' + i + ' = ' + (num2 * i));
+        i++;
     }
+
 }
-showMultiplicationTable(5);
-showMultiplicationTable(2);
+
+showMultiplicationTable(num1);
+
+
